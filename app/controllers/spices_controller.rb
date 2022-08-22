20a5a96134572585291d99090b93f7f67dea810c
youbaidjis <youba.etud@gmail.com>
2022-08-21 23:27:34 -0700

    Completed assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,41 @@
+class SpicesController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        spices = Spice.all 
+        render json: spices
+    end
+
+    def create
+        spice = Spice.create(spices_param)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = find_spice
+        spice.update(spices_param)
+        render json: spice
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_spice
+        Spice.find(params[:id])
+    end
+
+    def spices_param
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def render_not_found_response
+        render json: { error: "not found" }, status: :not_found
+      end
+
+end
